ecd87ca2fc513e3ad5fc0e0a710950575ace0226
Jose Egea Monserrate <1895784@alu.murciaeduca.es>
2021-11-25 00:56:40 +0100

    subida 1

---
+++ js/javascript.js
@@ -0,0 +1,89 @@
+
+
+const slider = document.querySelector(".slider");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll (".slide-icon");
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+
+//Boton siguiente slider
+
+nextBtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if (slideNumber > (numberOfSlides -1)){
+        slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+})
+
+//Boton previo slider
+
+prevBtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+    });
+
+    slideNumber--;
+
+    if (slideNumber < 0){
+        slideNumber = numberOfSlides -1;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+
+});
+
+//Autoplay sliders
+
+    var playSlider;
+
+    var repeater = () => {
+        playSlider = setInterval(function(){
+            slides.forEach((slide) => {
+                slide.classList.remove("active");
+            });
+            slideIcons.forEach((slideIcon) => {
+                slideIcon.classList.remove("active");
+            });
+        
+            slideNumber++;
+        
+            if (slideNumber > (numberOfSlides -1)){
+                slideNumber = 0;
+            }
+        
+            slides[slideNumber].classList.add("active");
+            slideIcons[slideNumber].classList.add("active");
+        }, 4000);
+
+    }
+    repeater ();
+
+//Parar imagen slider con mouse over
+
+slider.addEventListener("mouseover", () => {
+    clearInterval(playSlider);
+});
+
+//Empezar autoplay con mouseout
+
+slider.addEventListener("mouseout", () => {
+    repeater ();
+});
+
